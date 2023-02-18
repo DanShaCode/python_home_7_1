d7f38090ca886874c76f63fa4fa0356014fba6cc
Danis Sharipov <danshadance@mail.ru>
2023-02-19 02:01:17 +0600

    task_2 difficult

--- task_2.py
+++ task_2.py
@@ -4,7 +4,8 @@ import os
 # 2. Которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца. 
 # 3. Аргументы num_rows и num_columns указывают число строк и столбцов таблицы,которые должны быть распечатаны. 
 
-def print_operation_table():
+
+def print_operation_table(operation):
     os.system('cs||clear')
     print()
     table = [[1, 2, 3, 4, 5, 6],
@@ -17,7 +18,7 @@ def print_operation_table():
     for row in table:
         print(*row)
     print()
-
+    
     
     user_await = input("Нажмите Enter ")
     os.system('cs||clear')
